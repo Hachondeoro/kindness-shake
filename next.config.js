1efa8d9ae4db4de2968afe986e7d087e8a02c4e3
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2021-03-12 16:08:00 +0930

    environmental variables 1

--- next.config.js
+++ next.config.js
@@ -3,20 +3,24 @@ const withOffline = require('next-offline')
 module.exports = withOffline({
 	webpack: (config, options) => {
 		config.node = {
-		  // Some libraries import Node modules but don't use them in the browser.
-		  // Tell Webpack to provide empty mocks for them so importing them works.
-		  ...config.node,
-		  fs: "empty",
-		  child_process: "empty",
-		  net: "empty",
-		  tls: "empty",
-		};
-	
-		return config;
-	  },
+			// Some libraries import Node modules but don't use them in the browser.
+			// Tell Webpack to provide empty mocks for them so importing them works.
+			...config.node,
+			fs: 'empty',
+			child_process: 'empty',
+			net: 'empty',
+			tls: 'empty'
+		}
+
+		return config
+	},
+	env: {
+		sheets_email: process.env.sheets_email,
+		sheets_privatekey: process.env.sheets_privatekey
+	},
 	target: process.env.NEXT_TARGET || 'serverless',
 	images: {
-		domains: ['gw.alipayobjects.com'],
+		domains: ['gw.alipayobjects.com']
 	},
 	workboxOpts: {
 		swDest: 'static/service-worker.js',
