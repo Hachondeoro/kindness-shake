847ff2fa6182a31253a17003df247dfbd1d272c2
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2021-04-06 12:04:11 +0930

    improved volunteers form

--- pages/volunteer.jsx
+++ pages/volunteer.jsx
@@ -6,7 +6,10 @@ import Snackbar from '@material-ui/core/Snackbar'
 import MuiAlert from '@material-ui/lab/Alert'
 import ReactMarkdown from 'react-markdown'
 import VolunteerIntro from '@Components/VolunteerIntro.md'
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
 import * as emailjs from 'emailjs-com'
+import { Container } from 'reactstrap'
 
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 
@@ -56,6 +59,8 @@ const doc = new GoogleSpreadsheet(
 
 const VolunteerForm = props => {
 	const [form] = Form.useForm()
+	const [showConfetti, setshowConfetti] = useState(false)
+	const { width, height } = useWindowSize()
 
 	const sendEmail = e => {
 		emailjs
@@ -78,10 +83,11 @@ const VolunteerForm = props => {
 	const onFinish = values => {
 		const expertise = values.user.expertise.join(', ')
 		values.user.expertise = expertise
-		const availability = values.user.availability? "Yes":"No"
+		const availability = values.user.availability ? 'Yes' : 'No'
 		values.user.availability = availability
-		const consent = values.user.consent? "Yes":"No"
+		const consent = values.user.consent ? 'Yes' : 'No'
 		values.user.consent = consent
+		setshowConfetti(true)
 		sendEmail(values.user)
 		write_rows(values.user)
 		setOpen(true)
@@ -116,13 +122,13 @@ const VolunteerForm = props => {
 	return (
 		<>
 			<div className="home-page-wrapper">
-				<Row>
+				<Row align="middle" justify="center">
 					<Col
 						xs={{ span: 20 }}
 						lg={{ span: 8 }}
-						className="m-auto"
+						className="m-2"
 						align="left"
-						justify="right"
+						justify="center"
 					>
 						<div className="markdown">
 							<ReactMarkdown source={`${VolunteerIntro}`} />
@@ -133,27 +139,27 @@ const VolunteerForm = props => {
 					<Col
 						xs={{ span: 20 }}
 						lg={{ span: 8 }}
-						className="m-auto"
-						align="left"
+						className="m-2"
+						align="middle"
 						justify="center"
 					>
 						<img
 							src="/static/img/volunteer/volunteer1.jpg"
-							alt="frontcard"
+							alt="volunteer1"
 							width="80%"
 							height="80%"
 							className="mt-2"
 						/>
 						<img
 							src="/static/img/volunteer/volunteer2.jpg"
-							alt="frontcard"
+							alt="volunteer2"
 							width="80%"
 							height="80%"
 							className="mt-2"
 						/>
 						<img
 							src="/static/img/volunteer/volunteer3.jpg"
-							alt="frontcard"
+							alt="volunteer3"
 							width="80%"
 							height="80%"
 							className="mt-2 mb-4"
@@ -167,138 +173,190 @@ const VolunteerForm = props => {
 						variant="filled"
 						severity="success"
 					>
-						Thank you for your interest! We will be in touch with you soon :)
+						Thank you for your interest! We will be in touch with you soon
 					</MuiAlert>
 				</Snackbar>
-				<Row align="middle" justify="center" className="mx-5">
-					<Form
-						{...formItemLayout}
-						form={form}
-						name="nest-messages"
-						onFinish={onFinish}
-						validateMessages={validateMessages}
-						layout="horizontal"
-						initialValues={{
-							residence: ['zhejiang', 'hangzhou', 'xihu'],
-							prefix: '61'
-						}}
+				<div>
+					<Col
+						xs={{ span: 24 }}
+						lg={{ span: 20 }}
+						align="left"
+						justify="center"
 					>
-						<Form.Item
-							name={['user', 'firstname']}
-							label="First Name"
-							rules={[
-								{
-									required: true
-								}
-							]}
-						>
-							<Input />
-						</Form.Item>
-						<Form.Item
-							name={['user', 'lastname']}
-							label="Last Name"
-							rules={[
-								{
-									required: true
-								}
-							]}
-						>
-							<Input />
-						</Form.Item>
-						<Form.Item
-							name={['user', 'email']}
-							label="Email"
-							rules={[
-								{
-									type: 'email',
-									required: true
-								}
-							]}
-						>
-							<Input />
-						</Form.Item>
-						<Form.Item
-							name={['user', 'phone']}
-							label="Phone Number"
-							rules={[
-								{
-									required: true,
-									message: 'Please input your phone number!'
-								}
-							]}
-						>
-							<Input
-								addonBefore={prefixSelector}
-								style={{
-									width: '100%'
-								}}
+						{showConfetti ? (
+							<Confetti
+								width={width}
+								height={height}
+								numberOfPieces={1500}
+								gravity={0.08}
+								recycle={false}
+								onConfettiComplete={() => setshowConfetti(false)}
 							/>
-						</Form.Item>
-						<Form.Item name={['user', 'availability']} label="Available now?">
-							<Switch />
-						</Form.Item>
-						<Form.Item name={['user', 'position']} label="I want to be a">
-							<Radio.Group>
-								<Radio value="Casual">Casual Volunteer</Radio>
-								<Radio value="Permanent">Permanent Volunteer</Radio>
-							</Radio.Group>
-						</Form.Item>
-						<Form.Item
-							name={['user', 'expertise']}
-							label="Areas of expertise"
-							rules={[
-								{
-									required: true,
-									message: 'Please select your areas of expertise!',
-									type: 'array'
-								}
-							]}
-						>
-							<Select
-								mode="multiple"
-								placeholder="Please select your areas of expertise"
-							>
-								<Option value="Administration">Administration</Option>
-								<Option value="Event Management">Event Management</Option>
-								<Option value="IT">IT</Option>
-								<Option value="Logistics">Logistics</Option>
-								<Option value="Marketing">
-									Marketing, Communications and Media
-								</Option>
-								<Option value="Partnerships">Partnerships</Option>
-								<Option value="Fundraising">Fundraising</Option>
-								<Option value="Human Resources">Human Resources</Option>
-							</Select>
-						</Form.Item>
-						<Form.Item
-							className={'two-rows-label'}
-							name={['user', 'areasimprovement']}
-							label="Any other areas that you would like to develop professionally?"
-						>
-							<Input.TextArea />
-						</Form.Item>
-						<Form.Item
-							className={'two-rows-label'}
-							name={['user', 'consent']}
-							rules={[
-								{
-									validator: (_, value) =>
-										value
-											? Promise.resolve()
-											: Promise.reject(new Error('Should accept agreement'))
-								}
-							]}
-							label="I acknowledge that my personal details will be used for the purpose of expressing interest for Volunteering in Kindness Shake programs or events"
-						>
-							<Switch />
-						</Form.Item>
-						<Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-							<Button type="primary" htmlType="submit">
-								Submit
-							</Button>
-						</Form.Item>
-					</Form>
-				</Row>
+						) : null}
+
+						<div className="container">
+							<Row align="middle" justify="center" className="mx-5">
+								<Form
+									{...formItemLayout}
+									form={form}
+									name="nest-messages"
+									onFinish={onFinish}
+									validateMessages={validateMessages}
+									layout="horizontal"
+									initialValues={{
+										residence: ['zhejiang', 'hangzhou', 'xihu'],
+										prefix: '61'
+									}}
+								>
+									<Form.Item
+										name={['user', 'firstname']}
+										label="First Name"
+										rules={[
+											{
+												required: true
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+									<Form.Item
+										name={['user', 'lastname']}
+										label="Last Name"
+										rules={[
+											{
+												required: true
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+									<Form.Item
+										name={['user', 'preferredname']}
+										label="Preferred Name"
+										rules={[
+											{
+												required: true
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+									<Form.Item
+										name={['user', 'email']}
+										label="Email"
+										rules={[
+											{
+												type: 'email',
+												required: true
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+									<Form.Item
+										name={['user', 'phone']}
+										label="Phone Number"
+										rules={[
+											{
+												required: true,
+												message: 'Please input your phone number!'
+											}
+										]}
+									>
+										<Input
+											addonBefore={prefixSelector}
+											style={{
+												width: '100%'
+											}}
+										/>
+									</Form.Item>
+									<Form.Item
+										name={['user', 'address']}
+										label="Address"
+										rules={[
+											{
+												required: true
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+
+									<Form.Item name={['user', 'position']} label="I want to be a">
+										<Radio.Group>
+											<Radio value="Casual">Casual Volunteer</Radio>
+											<Radio value="Permanent">Permanent Volunteer</Radio>
+										</Radio.Group>
+									</Form.Item>
+									<Form.Item
+										name={['user', 'expertise']}
+										label="Areas of expertise"
+										rules={[
+											{
+												required: true,
+												message: 'Please select your areas of expertise!',
+												type: 'array'
+											}
+										]}
+									>
+										<Select
+											mode="multiple"
+											placeholder="Please select your areas of expertise"
+										>
+											<Option value="Administration">Administration</Option>
+											<Option value="Event Management">Event Management</Option>
+											<Option value="IT">IT</Option>
+											<Option value="Logistics">Logistics</Option>
+											<Option value="Marketing">
+												Marketing, Communications and Media
+											</Option>
+											<Option value="Partnerships">Partnerships</Option>
+											<Option value="Fundraising">Fundraising</Option>
+											<Option value="Human Resources">Human Resources</Option>
+											<Option value="Other">Other</Option>
+										</Select>
+									</Form.Item>
+									<Form.Item
+										className={'two-rows-label'}
+										name={['user', 'areasimprovement']}
+										label="Any other areas that you would like to develop professionally?"
+									>
+										<Input.TextArea />
+									</Form.Item>
+									<Form.Item
+										className={'two-rows-label'}
+										name={['user', 'whyus']}
+										label="Why do you want to volunteer with Kindness Shake?"
+									>
+										<Input.TextArea />
+									</Form.Item>
+									<Form.Item
+										className={'two-rows-label'}
+										name={['user', 'consent']}
+										rules={[
+											{
+												validator: (_, value) =>
+													value
+														? Promise.resolve()
+														: Promise.reject(
+																new Error('Should accept agreement')
+														  )
+											}
+										]}
+										label="I acknowledge that my personal details will be used for the purpose of expressing interest for Volunteering in Kindness Shake programs or events"
+									>
+										<Switch />
+									</Form.Item>
+									<Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+										<Button type="primary" htmlType="submit">
+											Submit
+										</Button>
+									</Form.Item>
+								</Form>
+							</Row>
+						</div>
+					</Col>
+				</div>
 			</div>
 		</>
 	)
--- src/Components/VolunteerIntro.md
+++ src/Components/VolunteerIntro.md
@@ -24,35 +24,31 @@ _You must not have a record of breaching or be currently involved in a matter wh
 &nbsp;  
 &nbsp;  
 
-**What is the role?**
-
-Volunteers participate in various activities throughout the year. You can check our Facebook Page to see the different events that we have held so far. You would be part of a team for one or more of those events and this can involve, for example, logistics, marketing, etc. You will have a team leader that will guide you at all times.
-&nbsp;  
-&nbsp;  
-
 **What are the benefits for you?**
 
 You will be able to:
 
 - Further develop your interpersonal, communication and leadership skills
 - Work in a team with your peers and other Kindness Shake Volunteers
-- Improve your employability and add the volunteer position to your resume
-- Use the program to count towards your professional career
+- Improve your employability skills and add the volunteer position to your resume
 - Make friends!
 &nbsp;  
 &nbsp;  
-**What is the commitment?**
 
-To participate in the Kindness Shake Volunteer program, you must:
+**We have two different types of volunteering positions:**
 
-- Be able to commit to the projects where possible and participate in a minimum of 3 events and activities
-- Attend any required training sessions and meetings
-- Provide a copy of Working with Children Check. If you do not have one, the Kindness Shake will cover the cost for you to obtain a Working with children check.
+**Casual Volunteer:** participating in our one-off, weekly or monthly projects and events. Expected commitment: 5-15 hours per project.
+
+**Permanent Volunteer:** being part of a KS portfolio (for example, marketing), with a specific role.   You will have a team leader that will guide you at all times.  Expected commitment: 6-12 months, at least 3 hours per week.
+&nbsp;  
+&nbsp;  
+
+For both positions you will be required to attend any required training sessions and meetings and you will also have to provide a copy of Working with Children Check. If you do not have one, the Kindness Shake will cover the cost for you to obtain one.  
 &nbsp;  
 &nbsp;  
 **How do I apply to become a Kindness Shake Volunteer?**
 
-Fill out the form below to register your expression of interest to become a Volunteer with Kindness Shake.
+Fill out the form below to register your expression of interest to become a Volunteer with Kindness Shake and one of our team members will contact you shortly.
 &nbsp;  
 &nbsp;  
 If you have any questions related to volunteering with Kindness Shake,
