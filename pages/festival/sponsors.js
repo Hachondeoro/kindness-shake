fe327307703b9995df7786268cfa87fa711422a4
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2021-07-17 22:09:13 +0930

    updated sponsors

--- pages/festival/sponsors.js
+++ pages/festival/sponsors.js
@@ -31,84 +31,70 @@ export async function getStaticProps() {
 }
 
 const Sponsors = ({ data }) => {
+	// const sponsors = [...new Set(data.allSponsors.map(obj => obj.class))]
+	const sponsors = [
+		'Platinum Sponsor',
+		'Gold Sponsor',
+		'Silver Sponsor',
+		'Media Partner'
+	]
+	const colorSponsors = ['#CACED8', '#d4af37', '#C0C0C0', '#000']
+	// console.log(sponsors)
 	return (
 		<>
 			<div className="home-page-wrapper">
 				<h1 className="text-center m-5">OUR SPONSORS</h1>
-				<h1 className="text-center m-5" style={{ color: '#d4af37' }}>
-					Gold Sponsors
-				</h1>
-				<div className="teams3-wrapper">
-					<div className="markdown">
-						<Row align="middle" justify="center">
-							{data.allSponsors
-								.filter(d => d.class === 'Gold Sponsor')
-								.map(item => (
-									<Col
-										xs={{ span: 12 }}
-										lg={{ span: 8 }}
-										// className="m-auto"
-										align="middle"
-										justify="center"
-									>
-										<a
-											href={item.hyperlink}
-											rel="noopener noreferrer"
-											target="_blank"
-										>
-											<img
-												src={item.image.url}
-												// width="100%"
-												width="90%"
-												style={{ margin: 'auto' }}
-												alt="img"
-											/>
-										</a>
-
-										<div className="font-weight-bold">
-											{item.title}
-										</div>
-									</Col>
-								))}
-						</Row>
-					</div>
-				</div>
-				<h1 className="text-center m-5">Media Partners</h1>
-				<div className="teams3-wrapper">
-					<div className="markdown">
-						<Row align="middle" justify="center">
-							{data.allSponsors
-								.filter(d => d.class === 'Media Partner')
-								.map(item => (
-									<Col
-										xs={{ span: 12 }}
-										lg={{ span: 8 }}
-										// className="m-auto"
-										align="middle"
-										justify="center"
-									>
-										<a
-											href={item.hyperlink}
-											rel="noopener noreferrer"
-											target="_blank"
-										>
-											<img
-												src={item.image.url}
-												// width="100%"
-												width="90%"
-												style={{ margin: 'auto' }}
-												alt="img"
-											/>
-										</a>
-
-										<div className="font-weight-bold">
-											{item.title}
-										</div>
-									</Col>
-								))}
-						</Row>
-					</div>
-				</div>
+				<Col
+					xs={{ span: 24 }}
+					lg={{ span: 14 }}
+					className="m-auto"
+					align="middle"
+					justify="center"
+				>
+					{sponsors.map((sponsorType, index) => {
+						return (
+							<>
+								<h1
+									className="text-center m-5"
+									style={{ color: colorSponsors[index] }}
+								>
+									{sponsorType}s
+								</h1>
+								{console.log(colorSponsors[index])}
+								<div className="teams3-wrapper">
+									<div className="markdown">
+										<Row align="middle" justify="center">
+											{data.allSponsors
+												.filter(d => d.class === sponsorType)
+												.map(item => (
+													<Col
+														xs={{ span: 12 }}
+														lg={{ span: 8 }}
+														// className="m-auto"
+														align="middle"
+														justify="center"
+													>
+														<a
+															href={item.hyperlink}
+															rel="noopener noreferrer"
+															target="_blank"
+														>
+															<img
+																src={item.image.url}
+																className="sponsorImages"
+																alt="img"
+															/>
+														</a>
+														<div className="font-weight-bold">{item.title}</div>
+													</Col>
+												))}
+										</Row>
+									</div>
+								</div>
+							</>
+						)
+					})}
+				</Col>
 				<br></br>
 			</div>
 		</>
